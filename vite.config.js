f35bb8786423e8b580ea632bd4865401e74bcd5c
Vincenzo De Leonardis <vincenzo.web.designer@gmail.com>
2024-10-16 10:20:47 +0200

    modified

--- vite.config.js
+++ vite.config.js
@@ -3,20 +3,34 @@ import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 import cors from 'cors';
 
-export default defineConfig({
-    // base: './quick-cms/public/',
-    plugins: [
-        laravel({
-            input: ['resources/css/admin/app.css', 'resources/css/quick_cms/app.css', 'resources/sass/app.scss', 'resources/js/app.jsx'],
-            refresh: true,
-        }),
-        react(),
-    ],
-    server: {
-        middleware: [
-            cors(),
+export default defineConfig(({ command, mode }) => {
+    let serverConfig = {};
+
+    if (command === 'serve') {
+        // Configurazione per l'ambiente locale
+        serverConfig = {
+            host: 'localhost',
+            port: 3000, // Usa 3000 in locale
+        };
+    } else {
+        // Configurazione per l'ambiente di produzione (es. su Render)
+        serverConfig = {
+            host: '0.0.0.0', // Per ascoltare tutte le interfacce
+            port: process.env.PORT || 3000, // Usa la variabile PORT in produzione
+        };
+    }
+
+    return {
+        plugins: [
+            laravel({
+                input: ['resources/css/admin/app.css', 'resources/css/quick_cms/app.css', 'resources/sass/app.scss', 'resources/js/app.jsx'],
+                refresh: true,
+            }),
+            react(),
         ],
-        host: '0.0.0.0',
-        port: process.env.PORT || 3000,
-    },
+        server: {
+            ...serverConfig, // Usa la configurazione in base all'ambiente
+            middleware: [cors()],
+        },
+    };
 });
