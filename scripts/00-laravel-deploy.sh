eb5c608aa076abe0b037c6b49cc4acbd6688887b
Vincenzo De Leonardis <vincenzo.web.designer@gmail.com>
2024-10-16 18:03:03 +0200

    update laravel-deploy.sh

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
+
+# Esegui i comandi necessari per il deploy
 echo "Running composer"
 composer install --no-dev --working-dir=/var/www/html
+
+echo "Generating application key..."
+php artisan key:generate --show --no-interaction
 
 echo "Caching config..."
 php artisan config:cache
@@ -9,4 +14,8 @@ echo "Caching routes..."
 php artisan route:cache
 
 echo "Running migrations..."
-php artisan migrate --force 
+php artisan migrate --force
+
+# Avvia Nginx in foreground
+echo "Starting Nginx..."
+nginx -g 'daemon off;'
