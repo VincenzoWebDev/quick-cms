5707ce93a29d0a814cd74e962e26eaecd65d8d40
Vincenzo De Leonardis <vincenzo.web.designer@gmail.com>
2026-03-03 16:21:26 +0100

    bux fix

--- resources/js/Layouts/AuthLayout.jsx
+++ resources/js/Layouts/AuthLayout.jsx
@@ -11,58 +11,59 @@ const AuthLayout = ({ title, subtitle, icon = 'fa-shield-halved', children, alte
   }, []);
 
   return (
-    <div className={`auth-shell ${demo_mode == 1 ? 'has-demo-banner' : ''}`}>
-      {demo_mode == 1 && <DemoModeBanner />}
+    <>
+      {Number(demo_mode) === 1 && <DemoModeBanner />}
+      <div className={`auth-shell ${Number(demo_mode) === 1 ? 'has-demo-banner' : ''}`}>
+        <div className="auth-bg-orb auth-bg-orb-left"></div>
+        <div className="auth-bg-orb auth-bg-orb-right"></div>
 
-      <div className="auth-bg-orb auth-bg-orb-left"></div>
-      <div className="auth-bg-orb auth-bg-orb-right"></div>
+        <div className="auth-shell-inner container-fluid">
+          <div className="row g-4 align-items-stretch justify-content-center">
+            <div className="col-xl-5 col-lg-6">
+              <aside className="auth-side-card">
+                <Link href={route('home')} className="auth-brand">
+                  <i className="fa-solid fa-cubes-stacked"></i>
+                  <span>Quick CMS</span>
+                </Link>
 
-      <div className="auth-shell-inner container-fluid">
-        <div className="row g-4 align-items-stretch justify-content-center">
-          <div className="col-xl-5 col-lg-6">
-            <aside className="auth-side-card">
-              <Link href={route('home')} className="auth-brand">
-                <i className="fa-solid fa-cubes-stacked"></i>
-                <span>Quick CMS</span>
-              </Link>
+                <h1>{title}</h1>
+                <p>{subtitle}</p>
 
-              <h1>{title}</h1>
-              <p>{subtitle}</p>
-
-              <div className="auth-side-points">
-                <div className="auth-side-point">
-                  <i className="fa-solid fa-bolt"></i>
-                  <span>Interfaccia amministrativa moderna e veloce</span>
+                <div className="auth-side-points">
+                  <div className="auth-side-point">
+                    <i className="fa-solid fa-bolt"></i>
+                    <span>Interfaccia amministrativa moderna e veloce</span>
+                  </div>
+                  <div className="auth-side-point">
+                    <i className="fa-solid fa-shield-halved"></i>
+                    <span>Accesso protetto e gestione account centralizzata</span>
+                  </div>
+                  <div className="auth-side-point">
+                    <i className="fa-solid fa-sliders"></i>
+                    <span>Esperienza coerente con il pannello di controllo</span>
+                  </div>
                 </div>
-                <div className="auth-side-point">
-                  <i className="fa-solid fa-shield-halved"></i>
-                  <span>Accesso protetto e gestione account centralizzata</span>
-                </div>
-                <div className="auth-side-point">
-                  <i className="fa-solid fa-sliders"></i>
-                  <span>Esperienza coerente con il pannello di controllo</span>
-                </div>
-              </div>
-            </aside>
-          </div>
+              </aside>
+            </div>
 
-          <div className="col-xl-5 col-lg-6">
-            <section className="auth-form-card">
-              <div className="auth-form-head">
-                <div className="auth-form-icon">
-                  <i className={`fa-solid ${icon}`}></i>
+            <div className="col-xl-5 col-lg-6">
+              <section className="auth-form-card">
+                <div className="auth-form-head">
+                  <div className="auth-form-icon">
+                    <i className={`fa-solid ${icon}`}></i>
+                  </div>
+                  {alternateAction && <div className="auth-form-alt-action">{alternateAction}</div>}
                 </div>
-                {alternateAction && <div className="auth-form-alt-action">{alternateAction}</div>}
-              </div>
 
-              {infoMessage && <div className="auth-inline-info">{infoMessage}</div>}
+                {infoMessage && <div className="auth-inline-info">{infoMessage}</div>}
 
-              {children}
-            </section>
+                {children}
+              </section>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
